0e6cbc4ef9adb2f764402115bd8cbaf8c78a4050
luizscarsi <luizaugustoscarsi@gmail.com>
2023-11-30 10:58:41 -0300

    feat: Add app interactive menu

--- algoritmos-2023.2/trabalho-final/app.py
+++ algoritmos-2023.2/trabalho-final/app.py
@@ -1,5 +1,23 @@
 from controllers.task_controller import add_task, list_tasks
 
-add_task()
-add_task()
-list_tasks()
+while True:
+    print("\n*** Sistema de Gerenciamento de Tarefas ***")
+    print("Digite a opção desejada:")
+    print("\t0 - Sair")
+    print("\t1 - Adicionar tarefa")
+    print("\t2 - Visualizar tarefas")
+    
+    opcao = int(input())
+    
+    if opcao == 0:
+        break
+    elif opcao == 1:
+        add_task()
+    elif opcao == 2:
+        print("1 - Tarefas ativas: ")
+        print("2 - Tarefas concluídas: ")
+        filtro = int(input())
+        if filtro == 1:
+            list_tasks("ativa")
+        else:
+            list_tasks("concluída")
--- algoritmos-2023.2/trabalho-final/controllers/task_controller.py
+++ algoritmos-2023.2/trabalho-final/controllers/task_controller.py
@@ -2,17 +2,19 @@ from models.Task import Task
 from datetime import datetime
 
 task_list = []
+task_counter = 0
 
 def add_task():
+    task_counter += 1
     task = Task()
     task.id = len(task_list)
 
-    print(f"Tarefa id: {len(task_list)}")
+    print(f"Tarefa id: {task_counter}")
     task.description = input("Descrição: ")
     task.deadline = datetime.strptime(input("Data de finalização(DD-MM-AAAA): "), "%d-%m-%y").strftime("%d-%m-%Y")
     print()
     task_list.append(task)
 
-def list_tasks():
+def list_tasks(filter):
     for i in range(len(task_list)):
         print(f"Tarefa {i}:\n  Id: {task_list[i].id}\n  Description: {task_list[i].description}\n  Data de finalização: {task_list[i].deadline}\n  Situação: {task_list[i].situation}")
