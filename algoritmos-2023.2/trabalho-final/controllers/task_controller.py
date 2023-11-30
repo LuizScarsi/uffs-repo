4789e865b05d1c18219684b0e91f6bd385b3c70c
luizscarsi <luizaugustoscarsi@gmail.com>
2023-11-30 11:23:01 -0300

    fix: Change task id declaration

--- algoritmos-2023.2/trabalho-final/controllers/task_controller.py
+++ algoritmos-2023.2/trabalho-final/controllers/task_controller.py
@@ -2,19 +2,15 @@ from models.Task import Task
 from datetime import datetime
 
 task_list = []
-task_counter = 0
 
 def add_task():
-    task_counter += 1
     task = Task()
-    task.id = len(task_list)
-
-    print(f"Tarefa id: {task_counter}")
+    print(f"Tarefa id: {task.id}")
     task.description = input("Descrição: ")
-    task.deadline = datetime.strptime(input("Data de finalização(DD-MM-AAAA): "), "%d-%m-%y").strftime("%d-%m-%Y")
+    task.deadline = datetime.strptime(input("Data de finalização(DD-MM-AAAA): "), "%d-%m-%y")
     print()
     task_list.append(task)
 
 def list_tasks(filter):
     for i in range(len(task_list)):
-        print(f"Tarefa {i}:\n  Id: {task_list[i].id}\n  Description: {task_list[i].description}\n  Data de finalização: {task_list[i].deadline}\n  Situação: {task_list[i].situation}")
+        print(f"Tarefa {i}:\n  Id: {task_list[i].id}\n  Description: {task_list[i].description}\n  Data de finalização: {task_list[i].deadline}\n  Situação: {task_list[i].situation}\n")
--- algoritmos-2023.2/trabalho-final/models/Task.py
+++ algoritmos-2023.2/trabalho-final/models/Task.py
@@ -1,5 +1,9 @@
 class Task:
-    id = None
+    id = 0
     description = None
     remaining_time = None
     situation = "ativa"
+    
+    def __init__(self):
+       Task.id += 1 
+       self.id = Task.id
